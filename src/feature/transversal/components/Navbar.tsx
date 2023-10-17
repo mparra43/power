e3952e5c4bb9e333714d119d3f05e67198c66b0b
lorena <lorepg310@gmail.com>
2023-10-17 16:42:03 -0500

    task

--- src/feature/transversal/components/Navbar.tsx
+++ src/feature/transversal/components/Navbar.tsx
@@ -5,10 +5,10 @@ export const Navbar = () => {
     <nav className="w-100 py-3 border-bottom shadow-sm border-rounder rounded d-flex justify-content-between">
       <h4 className="pl-3 .text-secondary">Baltimore!</h4>
       <div className='d-flex mx-2 mx-lg-5' style={{width:'10rem'}}>
-        <Link className='w-50' to='/inicio' >
+        <Link className='w-50 text-info fw-bold' to='/inicio' >
           Graficas
         </Link>
-        <Link className='w-50' to='/tareas' >
+        <Link className='w-50 text-info fw-bold' to='/tareas' >
           Tareas
         </Link>
       </div>
--- src/shared/components/Modal.tsx
+++ src/shared/components/Modal.tsx
@@ -11,8 +11,8 @@ export interface ModalProps {
 
 export const Modal: React.FC<ModalProps> = ({ children, onClose, show, title }) => {
   return (
-    <div className="modal" tabIndex={-1} role="dialog">
-      <div className="modal-dialog" role="document">
+    <div className="modal" tabIndex={-1} style={{display:`${show ?'block':'none'}`}}>
+      <div className="modal-dialog" >
         <div className="modal-content">
           <div className="modal-header">
             <h5 className="modal-title">{title}</h5>
